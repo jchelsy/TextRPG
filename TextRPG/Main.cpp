5772b979e5ddff01505886c3e9b514c1097537f8
PlasmaHound <jchelsy2001@gmail.com>
2020-10-09 20:28:11 -0700

    Add project files.

---
+++ TextRPG/Main.cpp
@@ -0,0 +1,18 @@
+#include <thread>
+#include <cstdlib>
+#include <utility>
+#include <sstream>
+
+#include "Character.h"
+
+int main() {
+	// Initialize local variables
+	std::string strChoice;  // userChoice
+	bool boolChoice;
+	std::string Name;
+	std::string Class;
+
+
+
+	return 0;
+}
