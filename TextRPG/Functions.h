e5760392cd40a14e2cb752d014f21be39080cbc2
PlasmaHound <jchelsy2001@gmail.com>
2020-10-09 20:33:40 -0700

    First commit.

--- TextRPG/Functions.h
+++ TextRPG/Functions.h
@@ -1,2 +1,16 @@
 #pragma once
 
+/*  Header Library Imports  */
+/*  ======================  */
+#include <iostream>		// Input/Output Library
+#include <string>		// String Library
+
+// Function Declaration(s) for file 'Choices.cpp'
+std::string userChoiceString(std::string text);
+char userChoiceChar();
+int userChoiceInteger();
+bool choiceYesNo(std::string, std::string);
+char choiceAB();
+char choiceABC();
+char choiceABCD();
+char choiceABCDE();
